432a5e5f394253684804230fb3592f7cfe2a6304
Emre Ferit Aslantaş <emreferitaslantas@hotmail.com>
2023-07-13 17:38:00 +0300

    example add

--- main.go
+++ main.go
@@ -1,43 +1,31 @@
-// EFA
-
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strings"
-)
-
-func OnPage(link string) string {
-	res, err := http.Get(link)
-	if err != nil {
-		log.Fatal(err)
-	}
-	content, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return string(content)
-}
+import "fmt"
 
 func main() {
-	result := OnPage("https://www.bbc.co.uk/search?q=arda&d=SEARCH_PS")
-	searchClassStart := "<p class=\"ssrcss-6arcww-PromoHeadline e1f5wbog6\"><span aria-hidden=\"false\">" //94000
-	firstTitleIndex := strings.Index(result, searchClassStart)
-	fmt.Println(firstTitleIndex)
-	fmt.Println(len(searchClassStart))
-	firstTitleIndex = firstTitleIndex + len(searchClassStart)
-	last := len(result)
-	result = result[firstTitleIndex:last]
+	// Bir string değişkeni tanımlama ve atama
+	message := "Merhaba, Dünya!"
+	fmt.Println(message)
 
-	searchClassEnd := "</span>" //1000
-	endTitleIndex := strings.Index(result, searchClassEnd)
+	// Bir tamsayı değişkeni tanımlama ve atama
+	number := 10
+	fmt.Println(number)
 
-	//firstTitleIndex := strings.Index(result, searchClass)
-	fmt.Println(firstTitleIndex)
-	fmt.Println(endTitleIndex)
-	fmt.Println(result[0:endTitleIndex])
+	// Bir float değişkeni tanımlama ve atama
+	pi := 3.14
+	fmt.Println(pi)
+
+	// Bir bool değişkeni tanımlama ve atama
+	isTrue := true
+	fmt.Println(isTrue)
+
+	// İki tamsayı değişkeninin toplamını hesaplama
+	a := 5
+	b := 3
+	sum := a + b
+	fmt.Println("Toplam:", sum)
+
+	// Bir karmaşık sayı değişkeni tanımlama ve atama
+	complexNum := complex(3, 2)
+	fmt.Println(complexNum)
 }
